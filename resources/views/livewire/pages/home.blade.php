0fea77aded6b1b4372f102101c5924b4cc140452
Alex Szeliga <alexszeliga@gmail.com>
2024-07-30 17:31:15 -0400

    rename welcome to home, add community edit route, add community index page

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -10,7 +10,7 @@ new #[Layout('layouts.app')] class extends Component {
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Edit Community') }}
+            {{ __('Social Overlaps Home') }}
         </h2>
     </x-slot>
     <div class="py-12">
@@ -19,10 +19,11 @@ new #[Layout('layouts.app')] class extends Component {
                 <div class="max-w-xl">
                     <header>
                         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                            Prolly Community Name
+                            Home of the Home Page
                         </h2>
+
                         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            A form to edit the community.
+                            Here, I will place home page content when it exists.
                         </p>
                     </header>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('auth')->group(function () {
-    Volt::route('/', 'pages.dashboard')
-        ->name('welcome');
+    Volt::route('/', 'pages.home')
+        ->name('home');
     Volt::route('profile', 'pages.profile')
         ->name('profile');
+    Volt::route('community/{$slug}', 'pages.community.edit')
+        ->name('community.edit');
 });
 
 require __DIR__.'/auth.php';
