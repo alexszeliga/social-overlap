2d86468f8ba35266a82597dd4f46f7e4b8a465a8
Alex Szeliga <alexszeliga@gmail.com>
2024-07-30 10:53:56 -0400

    add permissions, communities

--- database/migrations/2024_07_29_222125_create_communities_table.php
+++ database/migrations/2024_07_29_222125_create_communities_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('communities', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->timestamps();
+            $table->softDeletes('deleted_at', precision: 0);
         });
     }
 
