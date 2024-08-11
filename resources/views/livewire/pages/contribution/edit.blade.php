d0722182da5f859e125e43c251c9adf1d246ccb6
Alex Szeliga <alexszeliga@gmail.com>
2024-08-11 14:48:37 -0400

    create a select input, implement for communities

--- resources/views/livewire/pages/contribution/edit.blade.php
+++ resources/views/livewire/pages/contribution/edit.blade.php
@@ -1,19 +1,29 @@
 <?php
 
+
+use Livewire\Attributes\Url;
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 
 new #[Layout('layouts.app')] class extends Component {
-    protected string $pageTitle = "Create Contribution";
+    #[Url(as: 'community')]
+    public ?string $slug;
 };
 ?>
 
 <div>
     <x-header>
-        <x-h1>
-            {{ __($this->pageTitle) }}
-        </x-h1>
+        <x-h1>Create Contribution</x-h1>
     </x-header>
     <x-content-card>
+        <form wire:submit="save" class="space-y-6">
+            <div>
+                <x-select-input class="block w-full">
+                    @foreach(auth()->user()->communities as $community)
+                    <option{{ $slug === $community->slug ? ' selected ' : ' ' }}value="{{$community->slug}}">{{$community->name}}</option>
+                    @endforeach
+                </x-select-input>
+            </div>
+        </form>
     </x-content-card>
 </div>
