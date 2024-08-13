e49c8b0d93f7acfdb165c23669b2c7d60eb7e816
Alex Szeliga <alexszeliga@gmail.com>
2024-08-13 16:46:13 -0400

    make basic form to add contribution

--- resources/views/livewire/pages/contribution/edit.blade.php
+++ resources/views/livewire/pages/contribution/edit.blade.php
@@ -1,13 +1,32 @@
 <?php
 
+use App\Models\Community;
+use App\Models\Contribution;
+
+use Illuminate\Support\Facades\Auth;
 
 use Livewire\Attributes\Url;
-use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
+use Livewire\Volt\Component;
 
 new #[Layout('layouts.app')] class extends Component {
-    #[Url(as: 'community')]
-    public ?string $slug;
+    #[Url(as: 'community'), Validate('exists:communities,slug')]
+    public string $slug;
+
+    #[Validate('required|url')]
+    public string $url;
+
+    public function save() {
+        $this->validate();
+        $community = Community::where('slug', '=', $this->slug)->sole();
+        $contribution = Contribution::create([
+                'user_id' => Auth::user()->id,
+                'url' => $this->url,
+        ]);
+        $contribution->addCommunity($community);
+        // TODO: route use to conversation page
+    }
 };
 ?>
 
@@ -18,11 +37,24 @@ new #[Layout('layouts.app')] class extends Component {
     <x-content-card>
         <form wire:submit="save" class="space-y-6">
             <div>
-                <x-select-input class="block w-full">
+                <x-input-label>Select Community</x-input-label>
+                <x-select-input wire:model="slug" class="block w-full">
                     @foreach(auth()->user()->communities as $community)
-                    <option{{ $slug === $community->slug ? ' selected ' : ' ' }}value="{{$community->slug}}">{{$community->name}}</option>
+                    <option value="{{$community->slug}}">{{$community->name}}</option>
                     @endforeach
                 </x-select-input>
+                <x-input-error :messages="$errors->get('slug')" class="mt-2"/>
+
+            </div>
+            <div>
+                <x-input-label>Url</x-input-label>
+                <x-text-input wire:model="url" class="block w-full"/>
+                <x-input-error :messages="$errors->get('url')" class="mt-2"/>
+            </div>
+            <div>
+                <x-primary-button>
+                    Contribute
+                </x-primary-button>
             </div>
         </form>
     </x-content-card>
