e78d11473c1b48020964b9ab844a326ca11e2651
Alex Szeliga <alexszeliga@gmail.com>
2024-08-16 13:37:16 -0400

    add card border and h4 components

---
+++ resources/views/components/card-border.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge([ 'class' => 'py-3 px-3 border border-gray-300 dark:border-gray-500 rounded-lg mb-3' ])}}>
+    {{ $slot }}
+</div>
---
+++ resources/views/components/h4.blade.php
@@ -0,0 +1,3 @@
+<h4 {{ $attributes->merge(['class'=> 'text-xs font-light text-gray-900 dark:text-gray-400']) }}>
+    {{ $slot }}
+</h4>
