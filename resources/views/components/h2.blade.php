28d2556a657ba84f373387e3023c500282575135
Alex Szeliga <alexszeliga@gmail.com>
2024-08-17 19:02:19 -0400

    add margin to bottom of h2

--- resources/views/components/h2.blade.php
+++ resources/views/components/h2.blade.php
@@ -1,3 +1,3 @@
-<h2 {{ $attributes->merge(['class'=> 'text-lg font-medium text-gray-900 dark:text-gray-100']) }}>
+<h2 {{ $attributes->merge(['class'=> 'text-lg font-medium text-gray-900 dark:text-gray-100 mb-3']) }}>
     {{ $slot }}
 </h2>
