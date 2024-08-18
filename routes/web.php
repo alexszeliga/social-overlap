57e3a4ef9c8ad6d46ccf724b89abf086aa33f934
Alex Szeliga <alexszeliga@gmail.com>
2024-08-18 15:15:12 -0400

    make routes moar heirarchy

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::middleware('auth')->group(function () {
     Volt::route('/',                                    'pages.home')              ->name('home');
     Volt::route('u/profile',                            'pages.profile')           ->name('profile');
     Volt::route('u/conversations',                      'pages.conversation.index')->name('conversation.index');
-    Volt::route('c/create',                             'pages.community.edit')    ->name('community.create');
-    Volt::route('c',                                    'pages.community.index')   ->name('community.index');
     Volt::route('contribution/create',                  'pages.contribution.edit') ->name('contribution.create');
+    Volt::route('c',                                    'pages.community.index')   ->name('community.index');
+    Volt::route('c/create',                             'pages.community.edit')    ->name('community.create');
     Volt::route('c/{community:slug}',                   'pages.community.view')    ->name('community.view');
     Volt::route('c/{community:slug}/edit',              'pages.community.edit')    ->name('community.edit');
     Volt::route('c/{community:slug}/{contribution:id}', 'pages.conversation.view') ->name('conversation.view');
