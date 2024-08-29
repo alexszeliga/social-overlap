a52d7c1e29d63a57c8e213f6d2d18dc616ceabc4
Alex Szeliga <alexszeliga@gmail.com>
2024-08-29 11:02:50 -0400

    page message component

---
+++ resources/views/components/page-message.blade.php
@@ -0,0 +1,7 @@
+<div class="relative max-w-7xl mx-auto">
+    <div class="absolute left-0 right-0 sm:left-6 sm:right-6 md:left-6 md:right-6 lg:right-8 lg:left-8">
+        <div class="p-4 sm:p-8 p-4 bg-white dark:bg-gray-800 shadow sm:rounded-lg mt-1 drop-shadow-xl">
+            {{ $slot }}
+        </div>    
+    </div>
+</div>
